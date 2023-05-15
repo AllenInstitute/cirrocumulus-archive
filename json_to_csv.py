0186b0ea3e3a1655c51f2e6183cbaf501fd23505
dana-rocha <46910672+dana-rocha@users.noreply.github.com>
2023-05-15 16:57:05 -0700

    adds todos

--- json_to_csv.py
+++ json_to_csv.py
@@ -1,5 +1,10 @@
 import pandas as pd
+
+# class to read in json and write out CSV file
 df = pd.read_json('/Users/dana.rocha/Documents/Github/cirro_datasets/test-may15/cirro_datasets_test_150523.json')
 df.to_csv('/Users/dana.rocha/Documents/Github/cirro_datasets/test-may15/cirro_datasets_test_150523_formatted_out.csv', index = None)
 
+# class to clean dataset
+# loop through columns, add NA if value is empty
 
+# class to call, user arguments (argparse)
