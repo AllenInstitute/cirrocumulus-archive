96223ad4be4ae072302cceade6617dd7ca1a691d
dana-rocha <46910672+dana-rocha@users.noreply.github.com>
2023-05-16 14:47:54 -0700

    removes unused imports, adds comments

--- json_to_csv.py
+++ json_to_csv.py
@@ -1,12 +1,7 @@
-import pandas as pd
 import json
 import csv
 
 # class to read in json
-# write out CSV file
-
-# df = pd.read_json('/Users/dana.rocha/Documents/Github/cirro_datasets/test-may15/cirro_datasets_test_150523.json')
-# df.to_csv('/Users/dana.rocha/Documents/Github/cirro_datasets/test-may15/cirro_datasets_test_150523_formatted_out.csv', index = None)
 
 # Read file
 with open("example_dataset.json", "r") as jsonfile:
@@ -20,22 +15,23 @@ unpack_keys = ["_id", "last_updated"]
 
 headers = []
 
+# Clean dataset
 for index, row in enumerate(json_obj):
     for key, val in row.items():
         if key not in headers:
             headers.append(key)
+        # loop through rows, add NA if value is empty or empty list
         if val in empty_values:
             row[key] = "NA"
         if key in "_id" or key == "last_updated":
             for nested_key, nested_val in val.items():
                 row[key] = nested_val
 
+# Write out CSV file
 with open("formatted_out_3.csv", "w", encoding="UTF8", newline="") as fileout:
     writer = csv.DictWriter(fileout, fieldnames=headers)
     writer.writeheader()
     writer.writerows(json_obj)
 
-# class to clean dataset
-# loop through columns, add NA if value is empty
 
-# class to call, user arguments (argparse)
+# Call, user arguments (argparse)
