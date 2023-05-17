43635ed8a21ab82c27a2c8226bac0c30f5c847ca
dana-rocha <46910672+dana-rocha@users.noreply.github.com>
2023-05-16 18:30:53 -0700

    removes non-alphanumeric characters from values

--- json_to_csv.py
+++ json_to_csv.py
@@ -1,5 +1,8 @@
 import json
 import csv
+import re
+
+# TODO: convert to classes, add main to call classes/methods, user arguments (argparse)
 
 # Read in JSON file
 with open("/Users/dana.rocha/Documents/Github/cirro_datasets/test-may15/cirro_datasets_test_150523.json", "r") as jsonfile:
@@ -31,17 +34,19 @@ for index, row in enumerate(json_obj):
         if header not in row.keys():
             row[header] = "NA"
 
-# Unlist values of list type
-for index, row in enumerate(json_obj):
-    for key, val in row.items():
-        if isinstance(val, list) and val not in empty_values:
+for index, data_row in enumerate(json_obj):
+    for key, val in data_row.items():
+        if isinstance(val, str):
+            # Removes non-alphanumeric characters besides whitespace
+            new_val = re.sub(r'[^A-Za-z0-9 ]+', '', val)
+            data_row[key] = new_val
+        # Unlist values of list type
+        elif isinstance(val, list) and val not in empty_values:
             val = (",".join(str(item) for item in val))
-            row[key] = val
+            data_row[key] = val
 
-# # Write out CSV file
-with open("test_outfile_1.csv", "w", encoding="UTF8", newline="") as fileout:
+# Write out CSV file
+with open("test_outfile_all_3.csv", "w", encoding="UTF8", newline="") as fileout:
     writer = csv.DictWriter(fileout, fieldnames=headers)
     writer.writeheader()
     writer.writerows(json_obj)
-
-# Call, user arguments (argparse)
