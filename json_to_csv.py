62e7f9d07067fa26a9c72424327a60bbaa692021
dana-rocha <46910672+dana-rocha@users.noreply.github.com>
2023-05-18 09:33:33 -0700

    feat: adds write_to_csv function

--- json_to_csv.py
+++ json_to_csv.py
@@ -15,9 +15,6 @@ def load_json(path):
 def fill_empty_values(json_to_parse):
     json_obj = json.loads(json_to_parse)
 
-    # empty_values = ["", [''], [], "[]"]
-    # unpack_keys = ["_id", "last_updated"]
-
     # Add "NA" where there are empty values
     for index, row in enumerate(json_obj):
         for key, val in row.items():
@@ -25,22 +22,20 @@ def fill_empty_values(json_to_parse):
             if key not in HEADERS:
                 HEADERS.append(key)
 
-            # Loop through rows, add NA if value is empty or empty list
+            # Loop through rows, add NA if value is empty or an empty list
             if val in EMPTY_VALUES:
                 row[key] = "NA"
 
-            # if key in unpack_keys:
-                # Unnest "oid" and "date" values from dictionary
-                # for nested_key, nested_val in val.items():
-                    # row[key] = nested_val
     return json_obj
 
 def fill_missing_row_values(file_to_clean):
     # Fill in missing row values with NA
+    # Useful for when object has incorrect # of keys (missing a column)
     for index, row in enumerate(file_to_clean):
         for header in HEADERS:
             if header not in row.keys():
                 row[header] = "NA"
+
     return file_to_clean
 
 # def remove_nonalpha_chars(in_file):
@@ -50,25 +45,11 @@ def fill_missing_row_values(file_to_clean):
 #                 # Removes non-alphanumeric characters besides whitespace
 #                 new_val = re.sub(r'[^A-Za-z0-9 ]+', '', val)
 #                 data_row[key] = new_val
-#             # Unlist values of list type
-#             elif isinstance(val, list) and val not in EMPTY_VALUES:
-#                 val = (",".join(str(item) for item in val))
-#                 data_row[key] = val
-#         return in_file
+
+#     return in_file
 
 def flatten_nested_values(json_file_to_parse):
-    # empty_values = ["", [''], [], "[]"]
     unpack_keys = ["_id", "last_updated"]
-
-    # Dont uncomment
-    # for index, row in enumerate(json_obj):
-    #     for key, val in row.items():
-
-    #         if key in unpack_keys:
-    #             # Unnest "oid" and "date" values from dictionary
-    #             for nested_key, nested_val in val.items():
-    #                 row[key] = nested_val
-
 
     for index, data_row in enumerate(json_file_to_parse):
         for key, val in data_row.items():
@@ -76,11 +57,20 @@ def flatten_nested_values(json_file_to_parse):
             if key in unpack_keys:
                 for nested_key, nested_val in val.items():
                     data_row[key] = nested_val
-            # Unlist values of list type
+            # Unlist values of list type (e.g. ["10x multiome", "Smart-seq"] to "10x multiome,Smart-seq")
             elif isinstance(val, list) and val not in EMPTY_VALUES:
                 val = (",".join(str(item) for item in val))
                 data_row[key] = val
+
     return json_file_to_parse
+
+def write_to_csv(file_to_write):
+    # Write out CSV file
+    with open("testing_out.csv", "w", encoding="UTF8", newline="") as fileout:
+        writer = csv.DictWriter(fileout, fieldnames=HEADERS)
+        writer.writeheader()
+        writer.writerows(file_to_write)
+
 
 
 
@@ -89,15 +79,13 @@ def main():
     parsed_json = fill_empty_values(loaded_file)
     flattened_json = flatten_nested_values(parsed_json)
     filled_missing_rows = fill_missing_row_values(flattened_json)
-    print(filled_missing_rows)
+    write_to_csv(filled_missing_rows)
+    # non_alpha_removed = remove_nonalpha_chars(filled_missing_rows)
+    # print(non_alpha_removed)
+    # print(filled_missing_rows)
     # print("made it here")
     # print(HEADERS)
 
-# # Write out CSV file
-# with open("test_outfile_all_3.csv", "w", encoding="UTF8", newline="") as fileout:
-#     writer = csv.DictWriter(fileout, fieldnames=headers)
-#     writer.writeheader()
-#     writer.writerows(json_obj)
 
 if __name__ == "__main__":
     main()
