1a762b7610a6b6ae3c05b2644e9ea8c083fdecdf
dana-rocha <46910672+dana-rocha@users.noreply.github.com>
2023-05-19 11:22:11 -0700

    refactor: adds all methods to class

--- json_to_csv.py
+++ json_to_csv.py
@@ -35,7 +35,6 @@ class CirroJsonToCSV:
                 self.json_to_parse = json.load(jsonfile)
     
     def fill_empty_values(self):
-
         # Add "NA" where there are empty values
         for index, row in enumerate(self.json_to_parse):
             for key, val in row.items():
@@ -46,6 +45,43 @@ class CirroJsonToCSV:
                 # Loop through rows, add NA if value is empty or an empty list
                 if val in self.empty_values:
                     row[key] = "NA"
+
+        return self.json_to_parse
+
+    def fill_missing_row_values(self):
+        # Fill in missing row values with NA
+        # For when JSON object (dictionary) has incorrect number of keys (when a row in the dataset is missing a column)
+        for index, row in enumerate(self.json_to_parse):
+            for header in self.headers:
+                if header not in row.keys():
+                    row[header] = "NA"
+
+        return self.json_to_parse
+    
+    def remove_nonalpha_chars_species_col(self):
+        for index, data_row in enumerate(self.json_to_parse):
+            for key, val in data_row.items():
+                if isinstance(val, str) and key == "species":
+                    # Removes non-alphanumeric characters besides whitespace
+                    new_val = re.sub(r'[^A-Za-z0-9 ]+', '', val)
+                    data_row[key] = new_val
+
+        return self.json_to_parse
+
+    def flatten_nested_values(self):
+        unpack_keys = ["_id", "last_updated"]
+
+        for index, data_row in enumerate(self.json_to_parse):
+            for key, val in data_row.items():
+                # Unnest "oid" and "date" values from dictionary
+                if key in unpack_keys:
+                    for nested_key, nested_val in val.items():
+                        data_row[key] = nested_val
+                # Unlist values of list type (e.g. ["10x multiome", "Smart-seq"] to "10x multiome,Smart-seq")
+                elif isinstance(val, list) and val not in self.empty_values:
+                    val = (",".join(str(item) for item in val))
+                    data_row[key] = val
+
         return self.json_to_parse
 
     def write_to_csv(self):
@@ -55,41 +91,8 @@ class CirroJsonToCSV:
             writer.writerows(self.json_to_parse)
 
 
-# def fill_missing_row_values(file_to_clean):
-#     # Fill in missing row values with NA
-#     # Useful for when object has incorrect number of keys (when a row in the dataset is missing a column)
-#     for index, row in enumerate(file_to_clean):
-#         for header in HEADERS:
-#             if header not in row.keys():
-#                 row[header] = "NA"
 
-#     return file_to_clean
 
-# def remove_nonalpha_chars_species_col(in_file):
-#     for index, data_row in enumerate(in_file):
-#         for key, val in data_row.items():
-#             if isinstance(val, str) and key == "species":
-#                 # Removes non-alphanumeric characters besides whitespace
-#                 new_val = re.sub(r'[^A-Za-z0-9 ]+', '', val)
-#                 data_row[key] = new_val
-
-#     return in_file
-
-# def flatten_nested_values(json_file_to_parse):
-#     unpack_keys = ["_id", "last_updated"]
-
-#     for index, data_row in enumerate(json_file_to_parse):
-#         for key, val in data_row.items():
-#             # Unnest "oid" and "date" values from dictionary
-#             if key in unpack_keys:
-#                 for nested_key, nested_val in val.items():
-#                     data_row[key] = nested_val
-#             # Unlist values of list type (e.g. ["10x multiome", "Smart-seq"] to "10x multiome,Smart-seq")
-#             elif isinstance(val, list) and val not in EMPTY_VALUES:
-#                 val = (",".join(str(item) for item in val))
-#                 data_row[key] = val
-
-#     return json_file_to_parse
 
 # def write_to_csv(file_to_write):
 #     with open("cirro_datasets.csv", "w", encoding="UTF8", newline="") as fileout:
@@ -112,6 +115,9 @@ def main():
     initialize = CirroJsonToCSV("example_dataset.json")
     initialize.load_json()
     initialize.fill_empty_values()
+    initialize.fill_missing_row_values()
+    initialize.remove_nonalpha_chars_species_col()
+    initialize.flatten_nested_values()
     initialize.write_to_csv()
 
 if __name__ == "__main__":
